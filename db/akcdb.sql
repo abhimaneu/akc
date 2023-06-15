fec98745b592ee3f6feaf738405707e9e901cbe4
Abhimaneu N S <abhimaneu2001@gmail.com>
2023-06-15 19:17:50 +0530

    db

---
+++ db/akcdb.sql
@@ -0,0 +1,225 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jun 15, 2023 at 01:46 PM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `akcdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company`
+--
+
+DROP TABLE IF EXISTS `company`;
+CREATE TABLE IF NOT EXISTS `company` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `code` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `company`
+--
+
+INSERT INTO `company` (`id`, `name`, `code`) VALUES
+(1, 'Company 1', 'C1'),
+(2, 'Company 2', 'C2'),
+(3, 'Company 3', 'C3'),
+(4, 'Company 4', 'C4'),
+(5, 'Company 5', 'C5'),
+(8, 'Company 7', 'C7'),
+(7, 'Company 6', 'C6'),
+(9, 'Company 8', 'C8');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inpass`
+--
+
+DROP TABLE IF EXISTS `inpass`;
+CREATE TABLE IF NOT EXISTS `inpass` (
+  `no` int NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `source` varchar(125) NOT NULL,
+  `woc` varchar(125) NOT NULL,
+  `op` int NOT NULL,
+  `vehicleno` varchar(50) NOT NULL,
+  `extras` varchar(125) NOT NULL,
+  `type` varchar(10) NOT NULL DEFAULT 'inpass',
+  PRIMARY KEY (`no`)
+) ENGINE=MyISAM AUTO_INCREMENT=1059 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `inpass`
+--
+
+INSERT INTO `inpass` (`no`, `date`, `source`, `woc`, `op`, `vehicleno`, `extras`, `type`) VALUES
+(1058, '2023-06-15', 'Company 1', 'C1', 284, 'KL 23 BC 2982', '', 'inpass');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inpass_products`
+--
+
+DROP TABLE IF EXISTS `inpass_products`;
+CREATE TABLE IF NOT EXISTS `inpass_products` (
+  `inpass_no` int NOT NULL,
+  `product_name` varchar(50) NOT NULL,
+  `product_code` varchar(25) NOT NULL,
+  `product_bundle` int NOT NULL,
+  `product_desc` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `inpass_products`
+--
+
+INSERT INTO `inpass_products` (`inpass_no`, `product_name`, `product_code`, `product_bundle`, `product_desc`) VALUES
+(1058, 'WoodWall', 'WW1', 100, '23 X 48cm'),
+(1058, 'Wood', 'W2', 10, '42cm');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `outpass`
+--
+
+DROP TABLE IF EXISTS `outpass`;
+CREATE TABLE IF NOT EXISTS `outpass` (
+  `no` int NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `dest` varchar(125) NOT NULL,
+  `woc` varchar(125) NOT NULL,
+  `vehicleno` varchar(50) NOT NULL,
+  `extras` varchar(125) NOT NULL,
+  `type` varchar(10) NOT NULL DEFAULT 'outpass',
+  PRIMARY KEY (`no`)
+) ENGINE=MyISAM AUTO_INCREMENT=1005 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `outpass`
+--
+
+INSERT INTO `outpass` (`no`, `date`, `dest`, `woc`, `vehicleno`, `extras`, `type`) VALUES
+(1000, '2023-06-14', 'Company 6', 'C6', 'KL 23 BC 2982', 'Tranfser', 'outpass'),
+(1001, '2023-06-14', 'Company 1', 'C1', 'KL 23 BC 2982', '', 'outpass'),
+(1002, '2023-06-15', 'Company 1', 'C1', 'KL 33 BC 1921', 'Handle', 'outpass'),
+(1004, '2023-06-15', 'Company 1', 'C1', 'KL 33 BC 1920', '', 'outpass');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `outpass_products`
+--
+
+DROP TABLE IF EXISTS `outpass_products`;
+CREATE TABLE IF NOT EXISTS `outpass_products` (
+  `outpass_no` int NOT NULL,
+  `product_name` varchar(50) NOT NULL,
+  `product_code` varchar(25) NOT NULL,
+  `product_bundle` int NOT NULL,
+  `product_desc` varchar(100) NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `outpass_products`
+--
+
+INSERT INTO `outpass_products` (`outpass_no`, `product_name`, `product_code`, `product_bundle`, `product_desc`) VALUES
+(1000, 'B', 'B2', 123, 'X4'),
+(1000, 'A', 'A1', 1, 'A3'),
+(1002, 'A', 'A1', 12, 'Dx4'),
+(1002, 'A', 'A1', 123, 'D3X4');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE IF NOT EXISTS `products` (
+  `name` varchar(125) NOT NULL,
+  `code` varchar(125) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`name`, `code`) VALUES
+('E', 'E3'),
+('F', 'F1'),
+('WoodWall', 'WW1'),
+('Wood', 'W2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
+DROP TABLE IF EXISTS `profile`;
+CREATE TABLE IF NOT EXISTS `profile` (
+  `name` varchar(50) NOT NULL,
+  `wo` varchar(25) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `profile`
+--
+
+INSERT INTO `profile` (`name`, `wo`) VALUES
+('AK Coir', 'AK123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehicles`
+--
+
+DROP TABLE IF EXISTS `vehicles`;
+CREATE TABLE IF NOT EXISTS `vehicles` (
+  `type` varchar(50) NOT NULL,
+  `number` varchar(50) NOT NULL,
+  `owner` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `vehicles`
+--
+
+INSERT INTO `vehicles` (`type`, `number`, `owner`) VALUES
+('Pick-Up', 'KL 33 BC 1920', 'Company'),
+('Pick-Up', 'KL 33 BC 1921', 'Others'),
+('Pick-Up', 'KL 33 BC 1923', 'Others'),
+('Pick-Up', 'KL 33 BC 1924', 'Company'),
+('Pick-Up', 'KL 33 BC 1925', 'Company'),
+('Pick-Up', 'KL 33 BC 1926', 'Company'),
+('Pick-up', 'KL 05 B 2834', 'Others'),
+('Pick-up', 'KL 05 B 2834', 'Others');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
